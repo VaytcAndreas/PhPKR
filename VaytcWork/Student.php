c5987f0aa72f1e0fe47410fed7d9154eef4263da
VaytcAndreas <93351370+VaytcAndreas@users.noreply.github.com>
2022-10-29 17:11:42 +0200

    Update Student.php

--- VaytcWork/Student.php
+++ VaytcWork/Student.php
@@ -1,6 +1,11 @@
 <?php
 namespace College\VaytcWork;
-require_once "C:\openserver\domains\PhPKR\college\VaytcWork\Gruppa.php";
+spl_autoload_register(function ($class) {
+    require __DIR__ . DIRECTORY_SEPARATOR . implode(
+        DIRECTORY_SEPARATOR,
+        explode('\\', str_replace('VaytcWork', '', $class))
+    ) . '.php';
+});
 use \DateTime;
 
 
